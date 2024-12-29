39050ef613f2d82a0fa95af91345214da206fe42
BugHunter-X <askzenai@gmail.com>
2024-12-29 14:52:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello from Node.js!' }));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
+//Unhandled promise rejection
+const promise = new Promise((resolve,reject) => {
+    reject(new Error('Something went wrong'));
+});
+
+promise.catch(error => {
+    console.error('Error:', error.message);
+});
